8935706576014c786f4a8db0f83da306a1dcb536
knford01 <54276167+knford01@users.noreply.github.com>
2024-04-23 17:08:20 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+//Partial Rendering - One benefit of using layouts in Next.js is that on navigation, only the page components update while the layout won't re-render
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
