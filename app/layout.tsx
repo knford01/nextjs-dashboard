8ea7cc01783249f2ebc5fabe574f9919e3479172
knford01 <54276167+knford01@users.noreply.github.com>
2024-04-26 16:45:28 -0500

    End Chapter 16
    Adding Metadeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 //Partial Rendering - One benefit of using layouts in Next.js is that on navigation, only the page components update while the layout won't re-render
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
