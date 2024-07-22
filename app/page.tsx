033e384e40aa9e9147c17719c1b9498a31079d78
knford01 <54276167+knford01@users.noreply.github.com>
2024-07-22 17:40:27 -0500

    Web Site 3/4 Finished

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { EnvelopeIcon, ArrowRightCircleIcon } from '@heroicons/react/24/outline';
-
+import { EnvelopeIcon, ArrowRightCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import ContactForm from '@/app/ui/contactform';
 import '@/app/ui/background.css';
 
@@ -24,7 +23,7 @@ export default function Page() {
         {repeatedListItems}
       </ul>
 
-      <main className="min-h-screen flex flex-col items-center justify-center z-0 overflow-auto">
+      <main className="min-h-screen flex flex-col items-center justify-center z-0 overflow-auto ml-10 mr-10">
 
         <header className="fixed top-0 left-0 right-0 z-50 bg-[#26394e] text-white shadow">
           <div className="relative flex items-center justify-between p-2">
@@ -93,8 +92,9 @@ export default function Page() {
                 </Link>
                 <button
                   onClick={() => setMobileMenuOpen(false)}
-                  className="font-bold hover:text-white text-[#eb3c00]"
+                  className="font-bold hover:text-white text-[#eb3c00] flex items-center"
                 >
+                  <XMarkIcon className="h-5 w-5 mr-2" />
                   Close Menu
                 </button>
               </div>
@@ -102,36 +102,53 @@ export default function Page() {
           )}
         </header>
 
-        <section className="relative w-full h-screen bg-[#26394e] text-[#26394e] flex items-center justify-center">
-          <Image
-            src="/images/website/support1.jpg"
-            alt="AR-Source Support"
-            layout="fill"
-            objectFit="cover"
-            className="opacity-90"
-          />
-          <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <h2 className="text-4xl font-bold">Streamline Your Digital Transformation</h2>
-            <p className="text-2xl mt-4 font-semibold">With Our Suite Of Cloud-Based Software</p>
+        <section className="relative w-full h-screen flex items-center justify-center overflow-hidden mt-20">
+          {/* Image with rounded corners */}
+          <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-lg z-0">
+            <Image
+              src="/images/website/support1.jpg"
+              alt="AR-Source Support"
+              layout="fill"
+              objectFit="cover"
+              className="opacity-90"
+            />
+          </div>
+          {/* Text positioned at the top right of the image with a semi-transparent background on mobile */}
+          <div className="absolute top-0 right-0 md:p-10 p-4 z-10 bg-black bg-opacity-80 md:bg-transparent rounded-lg">
+            <h2 className="text-4xl font-bold text-right text-white">Streamline Your Digital Transformation</h2>
+            <p className="text-2xl mt-4 font-semibold text-right text-white">With Our Suite Of Cloud-Based Software</p>
           </div>
         </section>
 
-        <section className="relative w-full h-screen bg-[#26394e] text-[#26394e] flex items-center justify-center">
-          <Image
-            src="/images/website/process2-2.jpg"
-            alt="AR-Source Process"
-            layout="fill"
-            objectFit="cover"
-            className="opacity-90"
-          />
-          <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center border r-5 p-10">
-            <h2 className="text-4xl font-bold">Work with an expert team to accomplish your business goals</h2>
-            <hr className="my-4 border-t border-[#eb3c00]" />
-            <p className="text-2xl mt-4 font-semibold">Our process will swiftly transforms your concept into a Minimum Viable Product (MVP), laying the groundwork for success</p>
+        <section className="relative w-full h-screen flex items-center justify-center overflow-hidden mt-20">
+          {/* Image with rounded corners */}
+          <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-lg z-0">
+            <Image
+              src="/images/website/process2-2.jpg"
+              alt="AR-Source Process"
+              layout="fill"
+              objectFit="cover"
+              className="opacity-90"
+            />
+          </div>
+          {/* Text positioned centrally with an opaque background */}
+          <div className="absolute inset-0 flex items-center justify-center p-4 z-10">
+            <div className="bg-[#26394e] bg-opacity-80 rounded-lg p-6 md:p-10">
+              <h2 className="text-4xl font-bold text-white text-center">Work with an expert team to</h2>
+              <h2 className="text-4xl font-bold text-white text-center">accomplish your business goals</h2>
+
+              <hr className="my-4 border-t border-[#eb3c00]" />
+              <p className="text-2xl text-white font-semibold text-center">
+                Our process will swiftly transforms your concept into a
+              </p>
+              <p className="text-2xl text-white font-semibold text-center">
+                Minimum Viable Product (MVP), laying the groundwork for success
+              </p>
+            </div>
           </div>
         </section>
 
-        <section className="flex flex-col items-center mt-20 px-4 mb-4">
+        <section className="flex flex-col items-center mt-20 px-4 pb-20">
           <div className="w-full max-w-4xl bg-[#26394e] shadow-lg rounded-lg p-6">
             <h3 className="text-2xl font-bold text-center text-white">Services</h3>
             <hr className="my-4 border-t border-[#eb3c00]" />
@@ -145,7 +162,7 @@ export default function Page() {
             </ul>
           </div>
 
-          <div className="w-full max-w-4xl bg-[#26394e] shadow-lg rounded-lg p-6 mt-4">
+          <div className="w-full max-w-4xl bg-[#26394e] shadow-lg rounded-lg p-6 mt-10">
             <h3 className="text-2xl font-bold text-center text-white">Software Suite</h3>
             <hr className="my-4 border-t border-[#eb3c00]" />
             <ul className="space-y-2 text-center">
@@ -157,7 +174,7 @@ export default function Page() {
             </ul>
           </div>
 
-          <button onClick={openContactForm} className="p-2 bg-[#26394e] text-white rounded-md mt-4 flex items-center">
+          <button onClick={openContactForm} className="p-2 bg-[#26394e] text-white rounded-md mt-10 flex items-center">
             <EnvelopeIcon className="h-5 w-5 mr-2" />
             Contact Us
           </button>
--- app/ui/contactform.tsx
+++ app/ui/contactform.tsx
@@ -56,7 +56,7 @@ const ContactForm = ({ onClose }: { onClose: () => void }) => {
     };
 
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 m-2">
             <div className="relative w-full max-w-xl p-4 bg-white rounded-lg shadow-md max-h-screen overflow-y-auto text-[#26394e]">
                 <button
                     onClick={onClose}
